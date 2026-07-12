c3bcab5e374177770e0e4bdc880b0d489872402e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "virtual_desktop_workspace_application_group_associations_id" {
+  description = "Map of id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.id }
+}
 output "virtual_desktop_workspace_application_group_associations_application_group_id" {
   description = "Map of application_group_id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
   value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.application_group_id }
