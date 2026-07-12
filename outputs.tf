c6cefe35f5065470304031913b5aa5ae86b493ab
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "virtual_desktop_workspace_application_group_associations" {
-  description = "All virtual_desktop_workspace_application_group_association resources"
-  value       = azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations
-}
 output "virtual_desktop_workspace_application_group_associations_application_group_id" {
-  description = "List of application_group_id values across all virtual_desktop_workspace_application_group_associations"
-  value       = [for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : v.application_group_id]
+  description = "Map of application_group_id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.application_group_id }
 }
 output "virtual_desktop_workspace_application_group_associations_workspace_id" {
-  description = "List of workspace_id values across all virtual_desktop_workspace_application_group_associations"
-  value       = [for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : v.workspace_id]
+  description = "Map of workspace_id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.workspace_id }
 }
 
