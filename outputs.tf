ab64ed9f488087cbd4b26b668b57ca718b8fbdbe
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "virtual_desktop_workspace_application_group_associations_id" {
   description = "Map of id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
-  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.id }
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "virtual_desktop_workspace_application_group_associations_application_group_id" {
   description = "Map of application_group_id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
-  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.application_group_id }
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.application_group_id if v.application_group_id != null && length(v.application_group_id) > 0 }
 }
 output "virtual_desktop_workspace_application_group_associations_workspace_id" {
   description = "Map of workspace_id values across all virtual_desktop_workspace_application_group_associations, keyed the same as var.virtual_desktop_workspace_application_group_associations"
-  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.workspace_id }
+  value       = { for k, v in azurerm_virtual_desktop_workspace_application_group_association.virtual_desktop_workspace_application_group_associations : k => v.workspace_id if v.workspace_id != null && length(v.workspace_id) > 0 }
 }
 
